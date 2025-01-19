34ea6b55c44a06c6ea96b135806d1d43a97f238f
Facundo Chomczyk <fchomczyk@gmail.com>
2025-01-19 19:33:28 -0300

    feat: assets added - general clean up - routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import SearchView from '@/views/SearchView.vue'
+import WelcomeView from '@/views/WelcomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,15 +8,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: SearchView,
+      component: WelcomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/search',
+      name: 'search',
+      component: SearchView,
     },
   ],
 })
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const message = 'Welcome to Pokédex'
+const subtitle =
+  'The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.'
+</script>
+
+<template>
+  <div class="welcome">
+    <img src="@/assets/pikachu.png" alt="Pikachu" />
+    <h2>{{ message }}</h2>
+    <h3>{{ subtitle }}</h3>
+    <button>Get Started</button>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.welcome {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
