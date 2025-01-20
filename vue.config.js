5b05d2ff125dfef7a313d07297fae469a2d9affc
Facundo Chomczyk <fchomczyk@gmail.com>
2025-01-20 12:02:17 -0300

    chore: github actions

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+import { defineConfig } from '@vue/cli-service'
+export default defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/pokedex-global-66/' : '/',
 })
